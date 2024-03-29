1c589c7bc23e487d9a8928b04fbd2a9380627e47
Austin Merril <austinm9506@gmail.com>
2024-03-29 08:28:25 -0400

    Added the lmAPI and routing to the server.

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 import express from "express";
+import { lmRouter } from "./controller/lmApi.js"
 
 const app = express()
+
+// Middleware
+
+app.use("/", lmRouter)
 
 const PORT = process.env.port || 8080
 
