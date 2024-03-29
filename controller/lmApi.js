1372ad2c62323c6f0602327d4aaf202293f63b0c
Austin Merril <austinm9506@gmail.com>
2024-03-29 08:28:00 -0400

    Added an API call to be tested for posting a single image.

--- controller/lmApi.js
+++ controller/lmApi.js
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import express from "express";
+import multer from "multer";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import dotenv from "dotenv";
 
@@ -12,10 +13,17 @@ const BUCKET_REGION = process.env.BUCKET_REGION
 const ACCESS_KEY = process.env.ACCESS_KEY
 const SECRET_KEY = process.env.SECRET_KEY
 
+// Creates an express router for basic LM API functionality 
 const lmRouter = express.Router()
 
+// Handles multipart form data using multer.
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
+
+// Creates Prisma client
 const prisma = new PrismaClient()
 
+// Creates an S3 client
 const s3 = new S3Client({
   credentials: {
     accessKeyId: ACCESS_KEY,
@@ -24,15 +32,25 @@ const s3 = new S3Client({
   region: BUCKET_REGION
 })
 
-const params = {
-  Bucket: BUCKET_NAME,
-  Key: request.file.originalname,
-  Body: request.file.buffer,
-  ContentType: request.file.mimetype
-}
+// API route for uploading images
+lmRouter.post("/api", upload.single("image"), async (req, res) => {
+  console.log("req.body", req.body)
+  console.log("req.file", req.file)
 
-const command = new PutObjectCommand(params)
-await s3.send(command)
+  req.file.buffer
+
+  const params = {
+    Bucket: BUCKET_NAME,
+    Key: req.file.originalname,
+    Body: req.file.buffer,
+    ContentType: req.file.mimetype
+  }
+
+  const command = new PutObjectCommand(params)
+  await s3.send(command)
+
+  res.send({})
+})
 
 lmRouter.get("/api", async (req, res) => {
   try {
@@ -42,3 +60,4 @@ lmRouter.get("/api", async (req, res) => {
   }
 })
 
+export { lmRouter }
