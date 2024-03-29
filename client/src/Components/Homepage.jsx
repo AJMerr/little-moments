4163c8456edd96edfaee6c47b42aeb09ebf22969
Austin Merril <austinm9506@gmail.com>
2024-03-29 12:29:26 -0400

    Added more fields to the home page for basic functionality.

--- client/src/Components/Homepage.jsx
+++ client/src/Components/Homepage.jsx
@@ -3,18 +3,24 @@ import axios from "axios"
 
 function Homepage() {
   const [file, setFile] = useState()
+  const [title, setTitle] = useState()
+  const [description, setDescription] = useState()
 
   const submit = async event => {
     event.preventDefault()
 
     const formData = new FormData();
     formData.append("image", file)
+    formData.append("title", title)
+    formData.append("description", description)
     await axios.post("/api", formData, { headers: { 'Content-Type': 'multipart/form-data' } })
   }
 
   return (
     <form onSubmit={submit}>
       <input onChange={e => setFile(e.target.files[0])} type="file" accept="image/*"></input>
+      <input value={title} onChange={e => setTitle(e.target.value)} type="text" placeholder='Title'></input>
+      <input value={description} onChange={e => setDescription(e.target.value)} type="text" placeholder='Description'></input>
       <button type="submit">Submit</button>
     </form>
   )
