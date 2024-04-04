cdffcd6240e00c9688f40692155339293521b49c
Austin Merrill <austinm9506@gmail.com>
2024-04-04 06:47:49 -0400

    Added a useEffect to get all photos and their metadata. Also added in a simple return to display all images.

--- client/src/Components/Homepage.jsx
+++ client/src/Components/Homepage.jsx
@@ -1,10 +1,23 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import axios from "axios"
 
 function Homepage() {
   const [file, setFile] = useState()
   const [title, setTitle] = useState()
   const [description, setDescription] = useState()
+  const  [image, images] = useState([])
+
+  useEffect(() => {
+    const fetchAllImages = async () => {
+      try {
+        const res = await axios.get("/api")
+        images(res.data)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    fetchAllImages()
+  }, [])
 
   const submit = async event => {
     event.preventDefault()
@@ -17,12 +30,21 @@ function Homepage() {
   }
 
   return (
-    <form onSubmit={submit}>
-      <input onChange={e => setFile(e.target.files[0])} type="file" accept="image/*"></input>
-      <input value={title} onChange={e => setTitle(e.target.value)} type="text" placeholder='Title'></input>
-      <input value={description} onChange={e => setDescription(e.target.value)} type="text" placeholder='Description'></input>
-      <button type="submit">Submit</button>
-    </form>
+    <div>
+      <form onSubmit={submit}>
+        <input onChange={e => setFile(e.target.files[0])} type="file" accept="image/*"></input>
+        <input value={title} onChange={e => setTitle(e.target.value)} type="text" placeholder='Title'></input>
+        <input value={description} onChange={e => setDescription(e.target.value)} type="text" placeholder='Description'></input>
+        <button type="submit">Submit</button>
+      </form>
+
+      {image.map((val, key) => {
+        return (
+          <img src={val.s3Url} alt="test"/>
+        )
+      })}
+
+    </div>
   )
 }
 
