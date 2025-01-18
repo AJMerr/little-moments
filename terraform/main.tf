80c2ad54cb1453bddd9bb53aa2f7950bb5b15534
Austin Merrill <austinm9506@gmail.com>
2025-01-18 13:38:40 -0500

    Added EC2 configuration in main.tf as well as a VPC config. Added output for output.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -50,3 +50,59 @@ resource "aws_s3_bucket_cors_configuration" "lm_prod_bucket_cors_configuration" 
         allowed_origins = ["*"]
     }
 }
+
+resource "aws_instance" "lm_prod_instance" {
+  ami = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  key_name = var.key_pair_name
+  vpc_security_group_ids = [aws_security_group.lm_prod_security_group.id]
+  tags = {
+    Name = "lm_prod_instance"
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners = ["099720109477"]
+
+  filter {
+    name = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+}
+
+// Add this data source to get the default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+resource "aws_security_group" "lm_prod_security_group" {
+  name = "lm_prod_security_group"
+  description = "Security group for HTTP and SSH access"
+  vpc_id = data.aws_vpc.default.id
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {    
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "lm_prod_security_group"
+  }
+}
