ab5d708082615fb6425b921491e5d8b7ba635231
Austin Merrill <austinm9506@gmail.com>
2025-01-21 04:24:17 -0500

    Added cognito to my terraform infrastructure.

--- terraform/main.tf
+++ terraform/main.tf
@@ -182,3 +182,62 @@ resource "aws_db_subnet_group" "lm_prod_subnet_group" {
     Name = "lm_prod_subnet_group"
   }
 }
+
+# Cognito User Pool
+resource "aws_cognito_user_pool" "little_moments_pool" {
+  name = "little-moments-user-pool"
+
+  password_policy {
+    minimum_length    = 8
+    require_lowercase = true
+    require_numbers   = true
+    require_symbols   = true
+    require_uppercase = true
+  }
+
+  username_attributes = ["email"]
+  
+  email_configuration {
+    email_sending_account = "COGNITO_DEFAULT"
+  }
+
+  verification_message_template {
+    default_email_option = "CONFIRM_WITH_CODE"
+    email_subject = "Your Little Moments verification code"
+    email_message = "Your verification code is {####}"
+  }
+
+  schema {
+    attribute_data_type = "String"
+    name                = "name"
+    required           = true
+    mutable            = true
+
+    string_attribute_constraints {
+      min_length = 1
+      max_length = 256
+    }
+  }
+
+  account_recovery_setting {
+    recovery_mechanism {
+      name     = "verified_email"
+      priority = 1
+    }
+  }
+}
+
+# Cognito User Pool Client
+resource "aws_cognito_user_pool_client" "little_moments_client" {
+  name = "little-moments-client"
+
+  user_pool_id = aws_cognito_user_pool.little_moments_pool.id
+
+  generate_secret = false
+  
+  explicit_auth_flows = [
+    "ALLOW_USER_SRP_AUTH",
+    "ALLOW_REFRESH_TOKEN_AUTH",
+    "ALLOW_USER_PASSWORD_AUTH"
+  ]
+}
--- terraform/output.tf
+++ terraform/output.tf
@@ -9,3 +9,13 @@ output "instance_public_ip" {
 output "instance_id" {
   value = aws_instance.lm_prod_instance.id
 }
+
+output "cognito_pool_arn" {
+  description = "ARN of the Cognito User Pool"
+  value       = aws_cognito_user_pool.little_moments_pool.arn
+}
+
+output "cognito_pool_endpoint" {
+  description = "Endpoint of the Cognito User Pool"
+  value       = aws_cognito_user_pool.little_moments_pool.endpoint
+} 
