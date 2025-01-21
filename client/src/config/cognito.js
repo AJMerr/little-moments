594384d06a0b3d43ad0b1072f4fa5f04318e106a
Austin Merrill <austinm9506@gmail.com>
2025-01-21 04:54:20 -0500

    Potentially fixed cognito Auth pool error.

--- client/src/config/cognito.js
+++ client/src/config/cognito.js
@@ -2,8 +2,11 @@ import { Amplify } from 'aws-amplify'
 
 Amplify.configure({
   Auth: {
-    region: import.meta.env.VITE_AWS_REGION,
-    userPoolId: import.meta.env.VITE_COGNITO_USER_POOL_ID,
-    userPoolWebClientId: import.meta.env.VITE_COGNITO_CLIENT_ID,
+    Cognito: {
+      userPoolId: import.meta.env.VITE_COGNITO_USER_POOL_ID,
+      userPoolClientId: import.meta.env.VITE_COGNITO_CLIENT_ID,
+      signUpVerificationMethod: 'code',
+      region: import.meta.env.VITE_AWS_REGION
+    }
   }
 }) 
